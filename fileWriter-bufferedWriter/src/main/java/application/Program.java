ee8b75ca0f7d17ecac126af9722b1a39597dddf9
vonvit <sandrineviturn@gmail.com>
2026-03-16 16:11:41 -0300

    Acrescentado mais dados em relacao ao que ja tinha no arquivo txt com FileWriter e BufferedWriter

--- fileWriter-bufferedWriter/src/main/java/application/Program.java
+++ fileWriter-bufferedWriter/src/main/java/application/Program.java
@@ -8,7 +8,7 @@ public class Program {
     void main() {
         String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
         String path = "C:\\temp\\out.txt";
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
             for (String line : lines) {
                 bw.write(line);
                 bw.newLine();
